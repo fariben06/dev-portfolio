5578a036e97072688a531969adc332bf1ba743d1
farid <nicefarid06@hotmail.com>
2023-01-01 16:56:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# Project Title
+
+A brief description of what this project does and who it's for developer/designers built with React.
+
+### Description
+
+A simple portfolio template for developer/designers built with React. 
+
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+#### Get the code
+
+#### git clone: `https://github.com/fariben06/dev-portfolio.git`
+ 
+### Install required dependencies
+
+`npm install`
+
+
+### Start the server
+
+`npm start`
+
+### More
+
+### Modify pages content in  
+
+`src/content_option.js`.
+
+### Thanks
+
+If you like this portfolio template don't -forget give it a ⭐ 
---
+++ src/content_option.js
@@ -0,0 +1,185 @@
+const logotext = "FARID";
+const meta = {
+    title: "Farid Benhamouda",
+    description: "I’m Farid Benhamouda data scientist _ Full stack devloper, currently working in france",
+};
+
+const introdata = {
+    title: "I’m Farid Benhamouda",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "data scientist _ Full stack devloper, currently working in france",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Passionate about the realization and development of web projects, I naturally turned to a career as a web developer. Today I want to join a dynamic and young company, so I will be delighted to join your team as a web developer, I was able to develop many skills and strengthen existing capacities. Specifically, I increased code efficiency by more than 75% on three client projects. I also participated in a UX Design project. All these achievements will be very useful to the company",
+};
+const worktimeline = [{
+    jobtitle: "Web Developer of week",
+    where: "france",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Sophia-Antipolis france",
+    date: "2021",
+},
+{
+    jobtitle: "Developer of week",
+    where: "Nice France",
+    date: "2022",
+},
+];
+
+const skills = [{
+    name: "Html5",
+    value: 90,
+},
+{
+    name: "CSS3",
+    value: 85,
+},
+{
+    name: "Vue.js",
+    value: 80,
+},
+{
+    name: "Python",
+    value: 85,
+},
+{
+    name: "Django",
+    value: 75,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 85,
+},
+{
+    name: "Jquery",
+    value: 80,
+},
+{
+    name: "MySQL Database ",
+    value: 75,
+},
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Mobile Apps",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Wordpress Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
